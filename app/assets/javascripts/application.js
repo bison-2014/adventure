7cae88c6e6bf2c19a4faa0b0475028df5c02a264
Dalal Alrayes <dalrayes@gmail.com>
2015-02-09 11:19:03 -0600

    refactored points javascript

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -37,52 +37,40 @@ $(document).ready(function(){
         })
   })
 
+
+
+  var ROUTE_ID_MATCHER = /\d*$/;
+
+
+  function changePoints(target, category) {
+    var myUrl = $(target).attr('action')
+    var id = myUrl.match(ROUTE_ID_MATCHER)[0];
+    var selector = '#' + category + id + '_points';
+    $.ajax({type: 'POST', url: myUrl})
+      .done(function(response) {
+        var points = $(response).find(selector).text();
+        $(selector).text(points);
+      })
+  }
+
   $('.container').on("submit", '.like_comment_form', function(event){
     event.preventDefault();
-      var myUrl = $(this).attr('action')
-      var regex = /\d*$/;
-      var id = myUrl.match(regex)[0]
-      $.ajax({type: 'POST', url: myUrl})
-        .done(function(response) {
-          var points = $(response).find('#comment'+id+'_points').text()
-          $('#comment'+id+'_points').text(points)
-        })
-  })
+    changePoints(this, "comment");
+  });
+
   $('.container').on("submit", '.dislike_comment_form', function(event){
     event.preventDefault();
-      var myUrl = $(this).attr('action')
-      var regex = /\d*$/;
-      var id = myUrl.match(regex)[0]
-      $.ajax({type: 'POST', url: myUrl})
-        .done(function(response) {
-          var points = $(response).find('#comment'+id+'_points').text()
-          $('#comment'+id+'_points').text(points)
-        })
+    changePoints(this, "comment");
   })
 
   $('#replies').on("submit", '.like_reply_form', function(event){
     event.preventDefault();
-      var myUrl = $(this).attr('action')
-      var regex = /\d*$/;
-      var id = myUrl.match(regex)[0]
-      $.ajax({type: 'POST', url: myUrl})
-        .done(function(response) {
-          console.log(response)
-          var points = $(response).find('#reply'+id+'_points').text()
-          $('#reply'+id+'_points').text(points)
-        })
+    changePoints(this, "reply");
   })
 
   $('#replies').on("submit", '.dislike_reply_form', function(event){
     event.preventDefault();
-      var myUrl = $(this).attr('action')
-      var regex = /\d*$/;
-      var id = myUrl.match(regex)[0]
-      $.ajax({type: 'POST', url: myUrl})
-        .done(function(response) {
-          var points = $(response).find('#reply'+id+'_points').text()
-          $('#reply'+id+'_points').text(points)
-        })
+    changePoints(this, "reply")
   })
 
   // $('#new_reply').click(function(event){
